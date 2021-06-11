84bfddf23c5c1abb658f7de4f59b43a3819e8034
BIGJUNE <pjy970108@gmail.com>
2021-06-12 02:01:26 +0900

    210612_show_and_tell_upload

---
+++ model_imagetext/show_and_tell/README.md
@@ -0,0 +1,46 @@
+# show and tell
+
+## 파일설명
+
+#### 전처리
+- build_vocab.py : <start>, <end>, <pad>, <unk> .을 문장에 넣는 작업 및 toknize. pkl에 저장
+- dataHandler.py : 이미지와 caption dataloader 만드는 파일
+- preprocess.py : build_vocab과 비슷한 기능을 하는것 같은데 train 과정에서 bulid_vacab과 datahandler를 사용하기때문에 사용안하는것 같음
+
+#### 모델
+- model_cnn_rnn.py : 학습에 필요한 
+- Model.py : model_cnn_rnn.py에 사용될 encoder(ResNet-152), Decoder RNN(LsTM) 모델 구조를 담은 파일
+
+#### 학습 및 test
+- train.py : flickr data set을 train, val, test 나누고 train
+- test_model.py : model test
+
+#### baseline 진행상황
+현재 flicker 8k를 사용하여 진행 <br>
+<br>
+image file + caption.txt 로 구성되어 있으며 <br>
+위 코드에 적용하기 위해 caption.txt는 json 파일로 변환하여 진행 <br>
+caption.txt 의 형태 <br>
+columns    image        caption <br>
+           image_이름   문장 <br>
+
+but cuda of memory 문제로 인해 학습 진행이 안됨....
+
+
+
+## <해야할일>...
+생각보다 해둔게 없어서 죄송하네요...
+
+#### 1. 데이터 파일 받았을때:
+- resnet-152를 이용한 기존 모델로 show and tell baseline 만들기
+- encoder의 pretrained model을 사용하여 show and tell baseline 만들기?
+
+#### 2. 데이터 파일을 받지 않았을때 :
+- flickr 8 파일로 돌아가는 모델 만들기
+
+#### 2. 생각해볼문제
+- 결과 값을 어떻게 추천에 활용할 것인지? (text 유사도? encoder 부분만 사용한다거나?)
+
+
+<데이터 출처 : https://www.kaggle.com/kunalgupta2616/flickr-8k-images-with-captions>
+<코드 출저: https://github.com/Pillercottrer/radcap_project>
---
+++ model_imagetext/show_and_tell/preprocess/dataHandler.py
@@ -0,0 +1,165 @@
+import torch
+import torchvision.transforms as transforms
+import torch.utils.data as data
+import os
+import pickle
+import numpy as np
+import nltk
+from PIL import Image
+import random
+import json
+import h5py
+
+class FlickrDataset(data.Dataset):
+    """COCO Custom Dataset compatible with torch.utils.data.DataLoader."""
+
+    def __init__(self, root, json_imgs, vocab, transform = None):
+        """Set the path for images, captions and vocabulary wrapper.
+
+        Args:
+            root: image directory.
+            json: coco annotation file path.
+            vocab: vocabulary wrapper.
+            transform: image transformer.
+        """
+        """
+        self.root = root
+        self.coco = COCO(json)
+        self.ids = list(self.coco.anns.keys())
+        self.vocab = vocab
+        self.transform = transform
+        """
+
+        """
+        f = h5py.File('data.h5', 'r')
+        self.label_start_ix = f['label_start_ix']
+        self.label_end_ix = f['label_end_ix']
+        self.labels = f['labels']
+        self.images = f['images']
+        self.label_length = f['label_length']
+        """
+        
+        self.img_data = json_imgs
+        self.vocab = vocab
+        self.transform = transform
+        self.root = root
+
+
+    def __getitem__(self, index):
+        """Returns one data pair (image and caption)."""
+        vocab = self.vocab
+        paths = self.img_data['file_paths'].values()
+        caption = self.img_data['caption'].values()
+
+        image_tensor = torch.zeros(len(paths), 3, 224, 224)
+        for i, path in enumerate(paths):
+            image = Image.open(path).convert('RGB')
+            if self.transform is not None:
+                image = self.transform(image)
+            image_tensor[i] = image
+
+        # Convert caption (string) to word ids.
+        tokens = nltk.tokenize.word_tokenize(str(caption).lower())
+        caption = []
+        caption.append(vocab('<start>'))
+        caption.extend([vocab(token) for token in tokens])
+        caption.append(vocab('<end>'))
+        target = torch.Tensor(caption)
+
+        return image_tensor, target
+
+    def __len__(self):
+        return len(self.img_data)
+
+
+
+def make_weights_for_balanced_classes(images, nclasses):
+    count = [0] * nclasses
+    for item in images:
+        count[item[1]] += 1
+    weight_per_class = [0.] * nclasses
+    N = float(sum(count))
+    for i in range(nclasses):
+        weight_per_class[i] = N/float(count[i])
+    weight = [0] * len(images)
+    for idx, val in enumerate(images):
+        weight[idx] = weight_per_class[val[1]]
+    return weight
+
+def make_weights_for_balanced_classes(json_imgs, nclasses):
+    count = [0] * nclasses
+    for item in json_imgs:
+        if item['fracture'] == str(1): #hard coded classes '0' and '1'
+            count[1] += 1
+        else:
+            count[0] += 1
+
+    weight_per_class = [0.] * nclasses
+    N = float(sum(count))
+    for i in range(nclasses):
+        weight_per_class[i] = N/float(count[i])
+    weight = [0] * len(json_imgs)
+
+    for idx, img in enumerate(json_imgs):
+        if img['fracture'] == 1:
+            weight[idx] = weight_per_class[1]
+        else:
+            weight[idx] = weight_per_class[0]
+    return weight
+
+
+def collate_fn(data):
+    """Creates mini-batch tensors from the list of tuples (image, caption).
+
+    We should build custom collate_fn rather than using default collate_fn,
+    because merging caption (including padding) is not supported in default.
+    Args:
+        data: list of tuple (image, caption).
+            - image: torch tensor of shape (3, 256, 256).
+            - caption: torch tensor of shape (?); variable length.
+    Returns:
+        images: torch tensor of shape (batch_size, 3, 256, 256).
+        targets: torch tensor of shape (batch_size, padded_length).
+        lengths: list; valid length for each padded caption.
+    """
+    # Sort a data list by caption length (descending order).
+    data.sort(key=lambda x: len(x[1]), reverse=True)
+    images, captions = zip(*data)
+
+
+    num_imgs = [len(img_tuple) for img_tuple in images]
+    images_tensor = torch.zeros(len(images), max(num_imgs), 3, 224, 224)
+
+    for i, images_tup in enumerate(images):
+        images_tensor[i, :num_imgs[i], :, :, :] = images_tup
+
+    # Merge captions (from tuple of 1D tensor to 2D tensor).
+    lengths = [len(cap) for cap in captions]
+    targets = torch.zeros(len(captions), max(lengths)).long()
+    for i, cap in enumerate(captions):
+        end = lengths[i]
+        targets[i, :end] = cap[:end]
+    return images_tensor, targets, lengths
+
+
+def get_loader(root, json_imgs, vocab, transform, batch_size, shuffle, num_workers, ):
+    """Returns torch.utils.data.DataLoader for custom Flicker8k dataset."""
+    # Flicker8k caption dataset
+    flickr = FlickrDataset(root = root, json_imgs = json_imgs, vocab = vocab, transform = transform)
+    # Data loader for Flicker dataset
+    # This will return (images, captions, lengths) for each iteration.
+    # images: a tensor of shape (batch_size, 3, 224, 224).
+    # captions: a tensor of shape (batch_size, padded_length).
+    # lengths: a list indicating valid length for each caption. length is (batch_size).
+
+    #weights = make_weights_for_balanced_classes(json_imgs, 2)
+    #weights = torch.DoubleTensor(weights)
+    #sampler = torch.utils.data.sampler.WeightedRandomSampler(weights, len(weights))
+
+    data_loader = torch.utils.data.DataLoader(dataset=flickr,
+                                              batch_size=batch_size,
+                                              shuffle=shuffle,
+                                              num_workers=num_workers,
+                                              collate_fn=collate_fn)
+
+    return data_loader
