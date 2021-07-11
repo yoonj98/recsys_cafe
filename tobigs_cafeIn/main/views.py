64b40d8da45110fde8980b65f3e44205aaf25669
AnMinjun <good_day1234@naver.com>
2021-07-11 12:29:13 +0000

    add templeates

--- tobigs_cafeIn/main/views.py
+++ tobigs_cafeIn/main/views.py
@@ -3,20 +3,11 @@ from django.http import HttpResponse
 
 
 def index(request):
-    
-    
-    '''
-    코드
-    
-    final_list = []
-    '''
-    
-    
-    
-    
-    
+
     return render(request, 'main/main.html', {})
 
 
 def result(request):
+
+    
     return render(request, 'main/result.html', {})
